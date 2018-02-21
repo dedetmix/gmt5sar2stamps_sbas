cfb45c878a569506b1041f99585f8339567a69f3
dedetmix <dedetmix@yahoo.co.id>
2018-02-21 11:22:01 +0000

    Delete link_sbas.bash

--- link_sbas.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-rm real_*grd imag_*grd
-cat list_sbas | while read name prm_master prm_slave date_master date_slave
-do
-  echo $name  
-  ln -s ../../../intf_all/"$name"/real.grd real_"$name".grd 
-  ln -s ../../../intf_all/"$name"/imag.grd imag_"$name".grd 
-done
